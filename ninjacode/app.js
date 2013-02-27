31d253b1ec9b68999a4618a59910a7a507ef097b
Alejandro Romero <alejo8591@gmail.com>
2013-02-27 15:35:27 -0500

    update structure

--- ninjacode/app.js
+++ ninjacode/app.js
@@ -1,6 +1,6 @@
 
 /**
- * Module dependencies.
+ * Dependencia de modulos.
  */
 
 var express = require('express')
@@ -8,12 +8,27 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
+  /**
+   * Este es un controlador para Node.js mysql. 
+   * Está escrito en JavaScript, no requiere compilación, 
+   * y es 100% MIT licencia.
+  */
   , mysql = require('mysql');
 
 var app = express();
-
+/** Parámetros de configuración, se establecen con el método app.set(settingName, value). 
+ * - Las vistas se almacenan en la carpeta ./views,ademas   que el formato de templates es jade.
+ * - Modulos añadidos a nuestra aplicación para dotarla de más funcionalidad. 
+ *   Para usarlas se debe utilizar el método app.use(middleware). Vamos a usar:
+ *   - bodyParser: facilita el acceso al cuerpo de las peticiones (requests), si son `posts` de formularios, 
+ *     o si son json. Su sintaxis puede ser request.body.personName.
+ *   - methodOverride: puede utilizar <input type="hidden"> y modificar el método HTTP de la petición. 
+ *   - router: se encarga de gestionar las rutas.
+ *   - static: permite servir contenido estático, por ejemplo, CSS, imágenes, Javascript, etc. 
+       Todo lo que esté en la carpeta ./public se sirva como contenido estático.
+ */
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3535);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -23,11 +38,11 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
-
+// Anbiente de desarrollo para la aplicacion `development`
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
+// Estableciendo conexión con el motor de base de datos MySQL
 function BD(){
   var cliente = mysql.createConnection({
     user: 'root',
@@ -36,6 +51,7 @@ function BD(){
     port: 8891,
     database: 'ninjacodetv_noticias'
   });
+  // conectando con MySQL requerimiento de este motor
   cliente.connect();
   return cliente;
 }
@@ -44,13 +60,13 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 app.post('/guardar', function(req, res){
+  // instanciando objeto para la conexión con la base de datos
   var objBD = BD();
   objBD.query("INSERT INTO `noticias`(`titulo`,`contenido`) VALUES ('" + req.body.txtTitulo + "', '"+req.body.txtPublicacion+"')", function(error){
     if(error){
       console.log(error.message);
     }else{
-      console.log('Insertado');
-      res.render('Cargado correctamente');
+      console.log('Insertado correctamente en la base de datos');
      objBD.end();
     }
   });
